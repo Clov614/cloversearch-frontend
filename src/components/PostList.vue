578596b9c2463a7df948b36ba9ec1d1131d6100e
Clov614 <clov614@gmail.com>
2024-02-25 15:03:29 +0800

    feat: 初始化聚合搜索页面

---
+++ src/components/PostList.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import landscape from "../assets/landscape.png";
+</script>
+
+<template>
+  文章列表
+  <img :src="landscape" alt="landscape" class="landscape" />
+</template>
+
+<style scoped>
+.landscape {
+  width: 200px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Antd from "ant-design-vue";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(router).use(Antd).mount("#app");
